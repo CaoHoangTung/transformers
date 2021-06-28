0f1d3045551ac176f4f619b763b6fc8345a11f98
proxyyht <caohoangtung2001@gmail.com>
2021-06-28 16:29:01 +0700

    multi-gpu training

--- examples/pytorch/language-modeling/train-original.sh
+++ examples/pytorch/language-modeling/train-original.sh
@@ -1,23 +1,25 @@
-export MODEL_DIR=/data.local/all/tungch/gpt2-vn/transformers/examples/pytorch/language-modeling/gpt2-vn
-export DATA_PATH=/data.local/all/tungch/text-corpus
-export CUDA_VISIBLE_DEVICES=0
+export MODEL_DIR=/mnt/414g/tungch/gpt2-vn
+export DATA_PATH=/mnt/414g/tungch/data
+export CUDA_VISIBLE_DEVICES=0,1,2
 
-python ./run_clm_no_trainer.original.py \
---output_dir="./gpt2-vn/gptvn_22_06_01" \
+
+# python \
+accelerate launch --config_file accerate_config_01.json ./run_clm_no_trainer.original.py \
+--output_dir="./gpt2-vn/gptvn_28_06_00" \
 --model_type="gpt2" \
 --config_name=$MODEL_DIR \
 --tokenizer_name=$MODEL_DIR \
---add_prefix_space="True" \
+--add_prefix_space="False" \
 --train_file=$DATA_PATH/voz-text-train.txt \
 --validation_file=$DATA_PATH/voz-text-test-1k.txt \
 --block_size="256" \
---per_device_train_batch_size="4" \
+--per_device_train_batch_size="12" \
 --per_device_eval_batch_size="1" \
---learning_rate="2e-3" \
---gradient_accumulation_steps="16" \
+--learning_rate="3e-4" \
+--gradient_accumulation_steps="1" \
 --weight_decay="0.01" \
---num_warmup_steps="1000" \
---num_train_epochs="20" \
+--num_warmup_steps="100" \
+--num_train_epochs="10" \
 --logging_steps="200" \
 --log_dir="./gpt2-vn/runs" \
 --save_steps="2000" \
